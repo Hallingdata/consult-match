8d169adfa0ea5886fd5ee96269552e43263565e1
Asgeir Sognefest <sogasg@gmail.com>
2018-10-09 15:21:21 +0200

    Update to new version of Truffle + Remove deployment of unused contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,3 @@
-const GustavoCoinCrowdsale = artifacts.require("./GustavoCoinCrowdsale.sol")
-const GustavoCoin = artifacts.require("./GustavoCoin.sol")
 const SimpleStorage = artifacts.require("./SimpleStorage")
 const Jobs = artifacts.require("./Jobs")
 const Consultants = artifacts.require("./Consultants")
@@ -7,7 +5,7 @@ const Consultants = artifacts.require("./Consultants")
 module.exports = function(deployer, network, accounts) {
   const openingTime = web3.eth.getBlock("latest").timestamp + 2 // two secs in the future
   const closingTime = openingTime + 86400 * 20 // 20 days
-  const rate = new web3.BigNumber(1000)
+  const rate = new web3.utils.toBN(1000)
   const wallet = accounts[1]
 
   return deployer
@@ -15,22 +13,9 @@ module.exports = function(deployer, network, accounts) {
       return deployer.deploy(SimpleStorage)
     })
     .then(() => {
-      return deployer.deploy(GustavoCoin)
-    })
-    .then(() => {
       return deployer.deploy(Jobs)
     })
     .then(() => {
       return deployer.deploy(Consultants)
-    })
-    .then(() => {
-      return deployer.deploy(
-        GustavoCoinCrowdsale,
-        openingTime,
-        closingTime,
-        rate,
-        wallet,
-        GustavoCoin.address
-      )
     })
 }
