da564e64a36bb4d2777cae3b158ce6194e615126
Asgeir Sognefest <sogasg@gmail.com>
2018-10-16 18:01:00 +0200

    Remove unused stuff

--- contracts/GustavoCoin.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.19;
-
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
-
-contract GustavoCoin is MintableToken {
-  string public name = "GUSTAVO COIN";
-  string public symbol = "GUS";
-  uint8 public decimals = 18;
-}
