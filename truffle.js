ba23fee761d4077c99e930b928bc049f609e99e8
Asgeir Sognefest <sogasg@gmail.com>
2018-10-17 14:50:20 +0200

    Remove unused deployment network

--- truffle.js
+++ truffle.js
@@ -20,12 +20,6 @@ module.exports = {
       network_id: "*", // Match any network id
       gas: 500000,
     },
-    ropsten: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: 3,
-      gas: 4700000,
-    },
     "ropsten-infura": {
       provider: () =>
         new HDWalletProvider(
