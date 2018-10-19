760c79cc0c991d70d1eb236952eb28078318f2d9
Asgeir Sognefest <sogasg@gmail.com>
2018-10-19 13:27:03 +0200

    Add some  info to the home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,6 +5,10 @@ import {
   Theme,
   WithStyles,
   createStyles,
+  Card,
+  CardActions,
+  Button,
+  CardContent,
 } from "@material-ui/core"
 
 interface Props extends WithStyles<typeof styles> {}
@@ -15,6 +19,27 @@ const Home: React.SFC<Props> = ({ classes }) => {
       <Typography variant="h2" className={classes.header}>
         Open marketplace for consultants
       </Typography>
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography>
+            The consult-match platform is developed as an example of how
+            blockchain technology can be used in practice.
+          </Typography>
+          <Typography>
+            The platform consists of:
+            <ol>
+              <li>The ability for employers to post jobs.</li>
+              <li>The ability for consultants to publish their profile.</li>
+            </ol>
+            Then people can browse jobs and possible get in contact with
+            employers. And employers can browse consultant profiles and get in
+            contact if they find a good match.
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button variant="outlined">Check out the code on GitHub</Button>
+        </CardActions>
+      </Card>
     </>
   )
 }
@@ -24,6 +49,11 @@ const styles = ({  }: Theme) =>
     header: {
       marginTop: 100,
       textAlign: "center",
+    },
+    card: {
+      maxWidth: 700,
+      margin: "auto",
+      marginTop: 100,
     },
   })
 
